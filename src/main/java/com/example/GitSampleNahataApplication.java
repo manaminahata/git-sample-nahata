536deeebb4728a9dc0caa237e0943fff6dec99da
manaminahata <manami.nahata@rakus-partners.co.jp>
2021-11-01 14:27:56 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleNahataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNahataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNahataApplication.class, args);
+	}
+
+}
